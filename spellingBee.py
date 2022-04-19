cf690af941d2e8c7a65e54103d91e7de3f66f4bd
daniel <daniel.serrato@docet.org.mx>
2022-04-18 19:52:32 -0500

    First Commit

---
+++ spellingBee.py
@@ -0,0 +1,38 @@
+import networkzero as nw0
+
+#Size screen
+WIDTH = 1440
+HEIGHT = 750
+
+#Variables
+
+words = [] #Save the list of words in the text file
+
+#Getting the words from the document
+
+
+#address = nw0.advertise("spellingBee")
+#print(address)
+#nw0.send_news_to(address, "spellingBee", "Hello")
+
+#while True:
+ #   message = input("Type a message to send: ")
+  #  nw0.send_news_to(address, "spellingBee", message)
+
+    #if message != None:
+     #  topic, reply = nw0.wait_for_news_from(address, "spellingBee")
+      #  print(reply)
+       # message = None
+#word = ''
+
+#def draw():
+ #   global word
+  #  screen.clear()
+   # screen.draw.text(word, center=(WIDTH/2,HEIGHT/2), color = "green", fontsize = 200)
+
+#def on_key_down(key, mod, unicode):
+ #   global word
+  #  word = word + unicode
+
+#def update():
+
