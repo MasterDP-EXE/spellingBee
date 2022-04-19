0e70af823b54d7987153d89371713a5fc3a26492
daniel <daniel.serrato@docet.org.mx>
2022-04-18 21:31:53 -0500

    Reading words, and server service implemented

--- spellingBee.py
+++ spellingBee.py
@@ -4,35 +4,56 @@ import networkzero as nw0
 WIDTH = 1440
 HEIGHT = 750
 
-#Variables
+#Initiating server
+address = nw0.advertise("spellingBee")
 
+#Variables
 words = [] #Save the list of words in the text file
+answers = [] #List of students and their marks
+status = "" #String to display the status of the program
+counter = 0 #Counter of words
 
 #Getting the words from the document
+f = open("words.txt", "r")
+for w in f:
+    words.append(w.lower())
+f.close()
+
+#Deleting return character "/n"
+for r in range(0, len(words)):
+    words[r] = words[r][0:-1]
 
 
-#address = nw0.advertise("spellingBee")
-#print(address)
-#nw0.send_news_to(address, "spellingBee", "Hello")
+#Sending words to spellingBee Service
+nw0.send_news_to(address, "spellingBee", len(words)) #Sending words quantity
 
-#while True:
- #   message = input("Type a message to send: ")
-  #  nw0.send_news_to(address, "spellingBee", message)
+for w in words:
+    nw0.send_news_to(address, "spellingBee", w)
+    status = "Sending Words..."
 
-    #if message != None:
-     #  topic, reply = nw0.wait_for_news_from(address, "spellingBee")
-      #  print(reply)
-       # message = None
-#word = ''
+status = "Words sended"
 
-#def draw():
- #   global word
-  #  screen.clear()
-   # screen.draw.text(word, center=(WIDTH/2,HEIGHT/2), color = "green", fontsize = 200)
+def draw():
+    global status
 
-#def on_key_down(key, mod, unicode):
- #   global word
-  #  word = word + unicode
+    screen.clear()
+    screen.draw.text(status, center = (WIDTH/2,HEIGHT/2), color = "green", fontsize = 200)
 
-#def update():
+def on_key_down(key, mod, unicode):
+    global status, counter, words
+
+    if key == keys.RETURN:
+        counter = counter + 1
+        status = str(counter) + " word"
+
+    if counter == len(words):
+        status = "Finish!!!"
+
+def update():
+    global status
+
+    if status == "Finish!!!":
+        status = "Receiving answers"
+        address = nw0.advertise("spellingBee")
+        nw0.wait_for_news_from("spellingBee")
 
